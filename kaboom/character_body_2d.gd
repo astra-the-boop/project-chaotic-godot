a468dae4899ef3c767efd9992263f30208496a2f
Logan Bentley <logangbentley@gmail.com>
2025-03-15 09:55:38 +1300

    Burn off your McDonalds by walking

--- kaboom/character_body_2d.gd
+++ kaboom/character_body_2d.gd
@@ -7,8 +7,15 @@ const accel = 50
 const friction = 70
 const max_jump = 3
 const gravity = 120
+const reduce_width = 0.08
 
 var current_jumps = 1
+var width: float = 1:
+	get:
+		return width
+	set(value):
+		width = max(value, 1)
+		scale.x = width
 
 func _ready():
 	add_to_group("player")
@@ -17,6 +24,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir: Vector2 = input()
 	if input_dir != Vector2.ZERO:
 		accelerate(input_dir)
+		width -= delta * reduce_width
 	else:
 		add_friction()
 	player_movement()
